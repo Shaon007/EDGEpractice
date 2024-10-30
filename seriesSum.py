5d962b1e353f3d596776f043d771edf54b5cf970
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2024-10-30 16:28:53 +0600

    first commit

---
+++ seriesSum.py
@@ -0,0 +1,5 @@
+
+from seriesSumMod import seriesSum
+maxRange = int(input("Enter the outer range: "))
+
+print(f"The sum of numbers from 1 to {maxRange} is:", seriesSum(maxRange))
---
+++ seriesSumMod.py
@@ -0,0 +1,3 @@
+def seriesSum(n):
+    total = n * (n + 1) // 2
+    return total
